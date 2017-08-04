25c5c76b7817934785383a450eaae571b6b05ecf
Michel <pmichven@gmail.com>
2017-08-04 14:32:43 -0500

    Custom Footer and Remove edit link

--- includes/custom-functions.php
+++ includes/custom-functions.php
@@ -12,3 +12,13 @@
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+//* Remove the edit link
+add_filter ( 'genesis_edit_post_link' , '__return_false' );
+
+/* Change the footer text */
+add_filter('genesis_footer_creds_text', 'custom_footer_creds_filter');
+function custom_footer_creds_filter( $creds ) {
+	$creds = 'Copyright [footer_copyright] <a href="'.get_bloginfo( 'url' ).'">'.get_bloginfo( 'name' ).'</a>.';
+	return $creds;
+}
